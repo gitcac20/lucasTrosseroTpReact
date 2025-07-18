a66acc6ccf80f194c57721a70e916dba6dc45024
gitcac20 <ltros036@gmail.com>
2025-07-18 19:15:46 -0300

    Primer commit - proyecto listo

---
+++ src/auth/firebase.js
@@ -0,0 +1,163 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+
+
+
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDjf80FW8haHf5ecaczUMYkHV_Ri0PBI3g",
+  authDomain: "tpreact-ec858.firebaseapp.com",
+  projectId: "tpreact-ec858",
+  storageBucket: "tpreact-ec858.appspot.com",
+  messagingSenderId: "960754635830",
+  appId: "1:960754635830:web:bce8efd7e8cea7b7d37497",
+  measurementId: "G-XP3BCYW71W"
+};
+
+
+
+const app = initializeApp(firebaseConfig);
+
+//////////////////////////////////////////////////////////////////////
+///////////////// AUTENTICACIÓN DE USUARIOS FIREBASE//////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+
+export function crearUsuario(email, password){
+    return(
+        new Promise((res, rej) => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed up 
+                console.log("Credenciales", userCredential)
+                const user = userCredential.user;
+                console.log(user)
+                res(user)
+                // ...
+            })
+            .catch((error) => {
+                console.log(error.code, error.message)
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                rej(error)
+                // ..
+            });
+        })
+    )
+}
+
+auth.useDeviceLanguage()
+export function logearG(){
+    signInWithPopup(auth, provider)
+    .then((result) => {
+        console.log("test", result)
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+    }).catch((error) => {
+        console.log("test error", error )
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+    });
+}
+
+export function loginEmailPass(email, password){
+    return(
+        new Promise((res, rej) => {
+            signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                console.log("Credenciales", userCredential)
+                const user = userCredential.user;
+                console.log(user)
+                res(user)
+            })
+            .catch((error) => {
+                console.log(error.code)
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                rej(error)
+            });
+        })
+    )
+}
+/////////////////////////////////////////////////////////////////
+///////////////////// BASE DE DATOS FIRESTORE  //////// ////////
+////////////////////////////////////////////////////////////////
+
+import { addDoc, collection, getDocs, getFirestore } from "firebase/firestore";
+
+const db = getFirestore(app);
+
+export function crearProducto(name, imagen, price, description) {
+    return new Promise(async (res, rej) => {
+        try {
+        const docRef = await addDoc(collection(db, "productos"), {
+            name: name,
+            imagen: imagen,
+            price: price,
+            description: description
+        });
+
+        console.log("Document written with ID: ", docRef.id);
+        res(docRef)
+
+        } catch (e) {
+        console.error("Error adding document: ", e);
+        rej(e)
+        }
+    });
+}
+
+export function obtenerProductos() {
+    return(
+        new Promise(async (res, rej) => {
+                try {
+                    const querySnapshot = await getDocs(collection(db, "users"));
+                    
+                    const resultados = querySnapshot.docs.map(doc => {
+                        const data = doc.data();
+                        return {
+                            id: doc.id,
+                            name: data.name,
+                            imagen: data.imagen,
+                            price: data.price,
+                            description: data.description
+                        };
+                    });
+
+                    res (resultados);
+                } catch (error) {
+                    console.error("Error al obtener los usuarios:", error);
+                    rej (error);
+                }
+        })
+    )
+}
+
+/*crearProducto("test", "url", 23, "klasjdklsajdsaldkklasdljka").then(() => {
+    console.log("si")
+}).catch((error) => {
+    console.log(error)
+})*/
+
+/*obtenerProductos().then((prod) => {
+    console.log(prod)
+}).catch((error) => {
+    console.log(error)
+})*/
---
+++ src/layouts/Home.jsx
@@ -0,0 +1,16 @@
+import CarruselBootstrap from "../components/CarruselBootstrap"
+import Footer from "../components/Footer"
+import MainBootstrap from "../components/MainBootstrap"
+
+function Home() {
+
+    return(
+        <div>
+            <CarruselBootstrap/>
+            <MainBootstrap/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Home
